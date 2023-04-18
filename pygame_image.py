1a9deac334afeb7a48cd0440ac502bb2b24c8117
c0b20136 <c0b2013689@edu.teu.ac.jp>
2023-04-18 17:01:47 +0900

    ren6

--- pygame_image.py
+++ pygame_image.py
@@ -6,7 +6,9 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    kk_img = pg.transform.flip(pg.image.load("ex01/fig/3.png"), True, False)
+    kk_img = pg.image.load("ex01/fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]
 
     tmr = 0
 
@@ -16,7 +18,10 @@ def main():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        screen.blit(bg_img, [0, 0])
+        x = tmr
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [1600-x, 0])
+        screen.blit(kk_imgs[tmr%2],[300,200])
 
         pg.display.update()
         clock.tick(100)
